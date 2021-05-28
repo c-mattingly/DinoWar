bdd9d26b939ee6c0b1c99457bf957c31a41cbbed
Mike Mattingly <cmattingly1584@gmail.com>
2021-05-28 16:24:26 -0700

    adding constants and state variables to .js file//adding additional card back image

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 /*----- constants -----*/
-
+const deck = ["dA", "dK", "dQ", "dJ", "d10", "d09", "d08", "d07", "d06", "d05", "d04", "d03", "d02", "hA", "hK", "hQ", "hJ", "h10", "h09", "h08", "h07", "h06", "h05", "h04", "h03", "h02", "cA", "cK", "cQ", "cJ", "c10", "c09", "c08", "c07", "c06", "c05", "c04", "c03", "c02", "sA", "sK", "sQ", "sJ", "s10", "s09", "s08", "s07", "s06", "s05", "s04", "s03", "s02"];
 
 /*----- app's state (variables) -----*/
 
+const pile = {
+    player = [],
+    comp = []
+};
+
+const cardsLeft = {
+    player = [],
+    comp = []
+};
+
+const discardPile = {
+    player = [],
+    comp = []
+};
 
 /*----- cached element references -----*/
 
