f2b2bb67032719d33bfd2a0023e3995a4026afac
Mike Mattingly <cmattingly1584@gmail.com>
2021-05-28 12:59:34 -0700

    adding html, css, and pseudocode with js files

---
+++ js/main.js
@@ -0,0 +1,38 @@
+/*----- constants -----*/
+
+
+/*----- app's state (variables) -----*/
+
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+// Going to need Event Listener for Draw Button
+
+// and New Game Button
+
+/*----- functions -----*/
+
+// Place Init function to get page ready for start of game. 
+
+// Place the Render function 
+
+    // Have a shuffle function(Randomization)
+
+    // Then split up the deck into player and computer piles of 26 cards each. 
+
+// draw function that takes 1 card from playerPile and 1 card from compPile. 
+
+// cardsLeft function to keep an updated display of playerCardsLeft and compCardsLeft
+
+// function to update display with who won each hand (ex: "Computer takes 10 with Ace")
+
+    // if there is a tie then go into warPhase "I Declare War". 3 cards down 1 card up. then diplay winner of warPhase.
+
+    // function to place discarded cards in proper discardPiles(playerDiscard compDiscard)
+
+    // clear all the cards
+
+// function to determine winning condition (respective cardsLeft counter reaches 0)
